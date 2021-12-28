9de851f3ac458dd6a0b24e0037a657b4f274bc3d
Maxim <maxidebox@list.ru>
2021-12-28 13:38:49 +0400

    Создание Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+import React from "react";
+import Layout from "./hoc/Layout/Layout";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div style={{
+          width: 400,
+          border: '1px solid black'
+        }}>
+          <h1>Hello React</h1>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default App;

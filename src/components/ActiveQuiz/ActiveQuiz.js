052582bfd433ca1da8b4fa6dc66ee456a7b4a26d
Maxim <maxidebox@list.ru>
2021-12-29 14:39:03 +0400

    Список вопросов

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './ActiveQuiz.module.css';
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = (props) => {
   return (
@@ -14,12 +15,7 @@ const ActiveQuiz = (props) => {
         </small>
       </p>
 
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <AnswersList answers={props.answers} />
     </div>
   );
 }

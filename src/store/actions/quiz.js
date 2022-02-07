e508d658b97a30e642ad9009878f0a80232aec10
Maxim <maxidebox@list.ru>
2022-02-07 18:31:52 +0400

    104. Страница теста. Часть 2

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -3,8 +3,13 @@ import {
   FETCH_QUIZ_SUCCESS,
   FETCH_QUIZES_ERROR,
   FETCH_QUIZES_START,
-  FETCH_QUIZES_SUCCESS
+  FETCH_QUIZES_SUCCESS,
+  FINISH_QUIZ,
+  QUIZ_NEXT_QUESTION,
+  QUIZ_RETRY,
+  QUIZ_SET_STATE
 } from "./actionsTypes";
+import {bindReporter} from "web-vitals/dist/modules/lib/bindReporter";
 
 export function fetchQuizes() {
   return async dispatch => {
@@ -73,4 +78,77 @@ export function fetchQuizSuccess(quiz) {
     type: FETCH_QUIZ_SUCCESS,
     quiz: quiz
   }
+}
+
+// ----------------
+
+export function retryQuiz() {
+  return {
+    type: QUIZ_RETRY
+  }
+}
+
+// ----------------
+
+export function quizSetState(answerState, results) {
+  return {
+    type: QUIZ_SET_STATE,
+    answerState: answerState,
+    results: results
+  }
+}
+
+export function finishQuiz() {
+  return {
+    type: FINISH_QUIZ
+  }
+}
+
+export function quizNextQuestion(questionNumber) {
+  return {
+    type: QUIZ_NEXT_QUESTION,
+    questionNumber: questionNumber
+  }
+}
+
+export function quizAnswerClick(answerId) {
+  return (dispatch, getState) => {
+    const state = getState().quiz;
+
+    if (state.answerState) {
+      const key = Object.keys(state.answerState)[0];
+
+      if(state.answerState[key] === 'success') {
+        return;
+      }
+    }
+
+    const question = state.quiz[state.activeQuestion];
+    const results = state.results;
+
+    if(question.rightAnswerId === answerId) {
+      if(!results[question.id]) {
+        results[question.id] = 'success'
+      }
+
+      dispatch(quizSetState({[answerId]: 'success'}, results))
+
+      const timeout = window.setTimeout(() => {
+        if (isQuizFinished(state)) {
+          dispatch(finishQuiz());
+        } else {
+          dispatch(quizNextQuestion(state.activeQuestion + 1));
+        }
+
+        window.clearTimeout(timeout);
+      }, 1000);
+    } else {
+      results[question.id] = 'error';
+      dispatch(quizSetState({[answerId]: 'error'}, results))
+    }
+  }
+}
+
+function isQuizFinished(state) {
+  return state.activeQuestion + 1 === state.quiz.length;
 }
